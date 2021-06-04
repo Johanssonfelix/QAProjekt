ed8cf6990dd2cf06098ae14fc50a8eacc0375b28
Felix Johansson <Johanssonfelix@hotmail.com>
2021-06-04 09:47:36 +0200

    Start of Projekt

---
+++ src/LoanSystem.java
@@ -0,0 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoanSystem {
+   private static Logger logger = LogManager.getLogger(LoanSystem.class);
+    public static void main(String[] args) {
+
+
+        logger.trace("Hej");
+        logger.error("Problem");
+
+    }
+}
